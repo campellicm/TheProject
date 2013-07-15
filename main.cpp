f7066f307165e745a297f658888e0613fc218315
Alessandro Stagni <astagni@192.168.1.131>
2013-07-16 01:20:34 +0200

    "Main Added"

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: astagni
+ *
+ * Created on 16 luglio 2013, 1.19
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
